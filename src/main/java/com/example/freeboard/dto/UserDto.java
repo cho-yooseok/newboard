23ed09466b1c90fe5e2db978d57355060c9dd1b3
choyooseok <bluesbeat93@gmail.com>
2025-09-16 23:20:59 +0900

    add README.md

--- src/main/java/com/example/freeboard/dto/UserDto.java
+++ src/main/java/com/example/freeboard/dto/UserDto.java
@@ -16,14 +16,13 @@ import java.time.LocalDateTime;
 public class UserDto {
     private Long id;
     private String username;
-    private String role; // <-- UserRole에서 String으로 타입 변경
+    private String role;
     private LocalDateTime createdAt;
 
     public static UserDto fromEntity(User user) {
         UserDto dto = new UserDto();
         dto.setId(user.getId());
         dto.setUsername(user.getUsername());
-        // User 엔티티의 UserRole enum 값을 문자열로 변환하여 UserDto의 String role 필드에 설정
         dto.setRole(user.getRole().name());
         dto.setCreatedAt(user.getCreatedAt());
         return dto;
